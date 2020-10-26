b5d6555ed923aceb58ea03b63dc64047ecf90fa7
jenseneducation-adin-ehnsio <55272975+jenseneducation-adin-ehnsio@users.noreply.github.com>
2020-10-26 12:14:08 +0100

    final cloud build test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Continious deployment"/>
+    <HelloWorld msg="Testing again"/>
   </div>
 </template>
 
